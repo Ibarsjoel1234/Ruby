fa08ea0f10c04226418bf68f3c8e903882501afd
Ibarsjoel1234 <35426719+Ibarsjoel1234@users.noreply.github.com>
2020-09-12 01:16:45 +0530

    Add files via upload

---
+++ Authenication.rb
@@ -0,0 +1,50 @@
+users = [
+    {username: "joel", passowrd: "joel1" },
+    {username: "ibars", passowrd: "ibars1" },
+    {username: "mike", passowrd: "mike1" },
+    {username: "jones", passowrd: "jones1" },
+    {username: "john", passowrd: "john1" }
+    
+]
+
+def auth_user(user_name,pwd,list_of_records)
+    list_of_records.each do |user_record|
+        if user_record[:username] == user_name && user_record[:passowrd] == pwd
+            return user_record
+        end
+    end
+    return "Given data is incorrect"
+end
+
+
+puts "Welcome to the authentication"
+25.times {print "-"}
+puts
+puts "Details"
+5.times {print "-"}
+puts
+puts "The program check the username and password "
+puts "If wrong it will exist"
+puts
+
+
+attempt = 1
+while attempt < 4
+
+    puts "Enter the Name"
+    user_name = gets.chomp.downcase
+    puts "Enter the Password"
+    pwd = gets.chomp.downcase
+    result = auth_user(user_name,pwd,users)
+    puts result
+    puts "Press n if you want to exist or press any to continue"
+    input = gets.chomp.downcase
+    break if input == "n"
+    attempt += 1
+
+end
+
+puts "You have exceeded the number of attempts" if attempt == 4
+
+
+
